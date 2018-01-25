33c828e63fddd874bcd57f4951b79fd513cfe1cf
Nikolai Lugovoi <n.n.lugovoy@gmail.com>
2018-01-25 09:20:12 +0200

    #64gVv10I fix sending receipt to guests

--- app/mailers/transaction_mailer.rb
+++ app/mailers/transaction_mailer.rb
@@ -90,6 +90,7 @@ class TransactionMailer < ActionMailer::Base
         transaction[:listing_title]
       end
 
+
       premailer_mail(:to => seller_model.confirmed_notification_emails_to,
                      :from => community_specific_sender(community),
                      :subject => t("emails.new_payment.new_payment")) do |format|
@@ -135,8 +136,10 @@ class TransactionMailer < ActionMailer::Base
       else
         transaction[:listing_title]
       end
+      
+      recipient_emails = buyer_model.guest? ? [buyer_model.primary_email.address] : buyer_model.confirmed_notification_emails_to
 
-      premailer_mail(:to => buyer_model.confirmed_notification_emails_to,
+      premailer_mail(:to => recipient_emails,
                      :from => community_specific_sender(community),
                      :subject => t("emails.receipt_to_payer.receipt_of_payment")) { |format|
         format.html {
