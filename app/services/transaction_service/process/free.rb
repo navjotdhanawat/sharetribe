fd27657cfbda920ef07dcb1fd3fbbee4952c80c9
Nikolai Lugovoi <n.n.lugovoy@gmail.com>
2018-01-29 10:54:38 +0200

    #1uKuvX0K change text for auto-reject emails

--- app/services/transaction_service/process/free.rb
+++ app/services/transaction_service/process/free.rb
@@ -1,7 +1,7 @@
 module TransactionService::Process
   class Free
 
-    def create(tx:, gateway_fields:, gateway_adapter:, force_sync:)
+    def create(tx:, gateway_fields:, gateway_adapter:, force_sync:, auto: false)
       Result::Success.new({result: true})
     end
 
