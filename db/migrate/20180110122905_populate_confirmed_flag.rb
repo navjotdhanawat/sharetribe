62802be14332f14aa4dc04a39e67008d04384cfc
Aivils Štoss <aivils@ithouse.lv>
2018-03-09 15:38:57 +0200

    migrations

--- db/migrate/20180110122905_populate_confirmed_flag.rb
+++ db/migrate/20180110122905_populate_confirmed_flag.rb
@@ -1,4 +1,6 @@
 class PopulateConfirmedFlag < ActiveRecord::Migration[5.1]
+  class Person < ApplicationRecord
+  end
   def up
     Person.all.each do |person|
       if person.emails.where('confirmed_at IS NOT NULL').count > 0
