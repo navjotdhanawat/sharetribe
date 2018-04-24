dea12ed88abd8f010614a42d14148deff45d7af6
Nik <devnik@ithouse.lv>
2018-04-24 06:29:23 +0300

    #hAcZF9NM added phone and address with business name in import

--- app/models/person.rb
+++ app/models/person.rb
@@ -384,7 +384,7 @@ class Person < ApplicationRecord
   def can_delete_email(email)
     EmailService.can_delete_email(self.emails,
                                   email,
-                                  self.accepted_community.allowed_emails)[:result]
+                                  self.community.allowed_emails)[:result]
   end
 
   # Returns true if the person has global admin rights in Sharetribe.
